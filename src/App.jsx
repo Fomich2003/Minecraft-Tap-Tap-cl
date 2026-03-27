a8ec05504a73c24573f03c4afabd6949f81d5ae9
Fomich2003 <fomichdima1a@gmail.com>
2026-03-27 18:40:16 +0200

    add tg init

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 function App() {
+  const [initData, setInitData] = useState(null)
   useEffect(() => {
     if (window) {
       console.log(window)
       console.log(window.Telegram.WebApp)
+      setInitData(JSON.stringify(window.Telegram.WebApp.initData, null, 2))
     }
   }, [])
   return (
     <>
-
+      {initData && initData}
     </>
   )
 }
