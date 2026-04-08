cdf78b1c45967239369675137df8367f4c24d2ef
Fomich2003 <fomichdima1a@gmail.com>
2026-04-08 17:40:45 +0300

    add debug

--- src/utils/smartFetch.js
+++ src/utils/smartFetch.js
@@ -1,10 +1,12 @@
 import { API_CONF } from "../config/api.config"
 
 async function smartFetch(path, options = {}) {
+    const debug = {}
     try {
         const response = await fetch(API_CONF.baseUrl + path, options)
-
         const data = await response.json()
+
+        debug.data = data
 
         if (!response.ok) {
             if (!data.message) throw new Error("Негативна відповідь від сервера, і немає повідомлення про помилку")
@@ -19,7 +21,8 @@ async function smartFetch(path, options = {}) {
         return {
             success: false,
             message: "Ой! Щось пішло не так, спробуйте пізніше...",
-            error: error
+            error: error.message,
+            debug
         }
     }
 }
