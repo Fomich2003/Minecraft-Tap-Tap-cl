7146ecafe72e8fb062811386c95bbf46a7200bd7
Fomich2003 <fomichdima1a@gmail.com>
2026-05-08 17:43:37 +0300

    срфтпу ифыуГКД

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,5 +1,6 @@
 const API_CONF = {
-    baseUrl: `http://localhost:6788/api`
-}
+    // baseUrl: `http://localhost:6788/api`
+     baseUrl: `https://stump-facial-tannery.ngrok-free.dev/api`
+} 
 
 export { API_CONF }
