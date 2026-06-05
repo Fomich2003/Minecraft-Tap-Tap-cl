ad07b9e071f9183cd828fc5dc5c28b6482854595
Fomich2003 <fomichdima1a@gmail.com>
2026-06-05 17:46:51 +0300

    add console to Profile

--- src/components/ServerImage/ServerImage.jsx
+++ src/components/ServerImage/ServerImage.jsx
@@ -4,7 +4,12 @@ import { API_CONF } from "../../config/api.config"
 
 function ServerImage({ imgSrc, imgClass = "" }) {
     return (
-        <img className={`ServerImage ${imgClass}`} src={API_CONF.baseFileUrl + imgSrc} alt="photo" />
+        <>
+            <img className={`ServerImage ${imgClass}`} src={API_CONF.baseFileUrl + imgSrc} alt="photo" />
+            <p>{API_CONF.baseFileUrl + imgSrc}</p>
+        </>
+
+
     )
 }
 
