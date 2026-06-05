7abf58bd0f0eb477c4697761c26ba7d2a5bd478a
Fomich2003 <fomichdima1a@gmail.com>
2026-06-05 17:56:51 +0300

    change path in icons

--- src/components/ServerImage/ServerImage.jsx
+++ src/components/ServerImage/ServerImage.jsx
@@ -1,12 +1,14 @@
 import "./ServerImage.scss"
 import { API_CONF } from "../../config/api.config"
+import statickImage from '../../../public/assets/icons/public/assets/icons/image.png';
 
 
 function ServerImage({ imgSrc, imgClass = "" }) {
     return (
         <>
-            <img className={`ServerImage ${imgClass}`} src={API_CONF.baseFileUrl + imgSrc} alt="photo" />
-            <p>{API_CONF.baseFileUrl + imgSrc}</p>
+            {/* <img className={`ServerImage ${imgClass}`} src={API_CONF.baseFileUrl + imgSrc} alt="photo" /> */}
+            <img className={`ServerImage ${imgClass}`} src={statickImage} alt="photo" />
+
         </>
 
 
