dfe55a5bf37f62d24d4bf3e460b9c7c5a866b935
Fomich2003 <fomichdima1a@gmail.com>
2026-06-05 18:01:09 +0300

    chenge statick image

--- src/components/ServerImage/ServerImage.jsx
+++ src/components/ServerImage/ServerImage.jsx
@@ -1,13 +1,11 @@
 import "./ServerImage.scss"
 import { API_CONF } from "../../config/api.config"
-import statickImage from '../../../public/assets/icons/public/assets/icons/image.png';
-
 
 function ServerImage({ imgSrc, imgClass = "" }) {
     return (
         <>
             {/* <img className={`ServerImage ${imgClass}`} src={API_CONF.baseFileUrl + imgSrc} alt="photo" /> */}
-            <img className={`ServerImage ${imgClass}`} src={statickImage} alt="photo" />
+            <img className={`ServerImage ${imgClass}`} src="/assets/icons/public/assets/icons/image.png" alt="photo" />
 
         </>
 
