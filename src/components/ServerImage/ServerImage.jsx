b2998f7e539757f46db887766705d1df49ed3225
Fomich2003 <fomichdima1a@gmail.com>
2026-06-10 17:34:49 +0300

    change ServerImage

--- src/components/ServerImage/ServerImage.jsx
+++ src/components/ServerImage/ServerImage.jsx
@@ -4,9 +4,8 @@ import { API_CONF } from "../../config/api.config"
 function ServerImage({ imgSrc, imgClass = "" }) {
     return (
         <>
-            {/* <img className={`ServerImage ${imgClass}`} src={API_CONF.baseFileUrl + imgSrc} alt="photo" /> */}
-            <img className={`ServerImage ${imgClass}`} src="/assets/icons/public/assets/icons/image.png" alt="photo" />
-
+            <img className={`ServerImage ${imgClass}`} src={API_CONF.baseFileUrl + imgSrc} alt="photo" />
+            <p>{API_CONF.baseFileUrl + imgSrc}</p>
         </>
 
 
